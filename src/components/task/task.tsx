6ab94e2a1100ff971d972f76726f68939b3e9d8f
Luis H. Moreno <luishmcmoreno@gmail.com>
2017-08-28 22:36:17 -0300

    fixing completed tasks

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,18 +1,36 @@
-import { Component, Prop, Element } from '@stencil/core';
+import { Component, Prop, State, Element } from '@stencil/core';
 
 @Component({
   tag: 'todo-task',
   styleUrl: 'task.scss'
 })
 export class TodoList {
-    @Prop() title: string;
-    @Prop() completed: boolean = false;
-    @Element() element: HTMLElement;
+  @Prop() title: string;
+  @Prop() completed: boolean = false;
+  @State() _completed: boolean = false;
+  @Element() element: HTMLElement;
+
+  private applyClass(): void {
+    this._completed = !this._completed;
+    console.log(this._completed);
+    const li = this.element.querySelector('li');
+    if (this._completed) {
+      li.classList.add('completed');
+    } else {
+      li.classList.remove('completed');        
+    }
+  }
+
+  componentDidLoad() {
+    this._completed = !this.completed;
+    this.applyClass();
+  }
+
 
   render() {
     return (
-      <li class={ this.completed ? '"completed"' : '"todo"' }>
-        <input type="checkbox" checked={ this.completed } />
+      <li>
+        <input type="checkbox" checked={ this._completed } onChange={ () => this.applyClass() } />
         {this.title}
       </li>
     );
